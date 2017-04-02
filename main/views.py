719bd1678d512ec9047373960cba31802b29e3c2
dmattia <dmattia@nd.edu>
2017-04-01 22:07:23 -0400

    Cleaned up view file

--- main/views.py
+++ main/views.py
@@ -7,7 +7,6 @@ from .models import BoolQuestion, BoolResponse, MCQuestion, MCOption, MCResponse
 import os
 import subprocess
 import random
-import html
 
 def render_elm(request, html_template, html_context, elm_template, elm_context):
   """ Renders elm context into an elm file, calls `elm-make` on that elm file, and
@@ -26,7 +25,7 @@ def render_elm(request, html_template, html_context, elm_template, elm_context):
 
   # Call elm-make on the rendered file
   # TODO: Only debug is settings.debug
-  return_code = subprocess.call("elm-make " + elm_filename + " --debug --output " + js_filename, shell=True)
+  return_code = subprocess.call("elm-make " + elm_filename + " --yes --debug --output " + js_filename, shell=True)
 
   # Exit if failed to build
   if return_code != 0:
@@ -48,27 +47,7 @@ def render_elm(request, html_template, html_context, elm_template, elm_context):
 
 # Create your views here.
 class HomeView(View):
-  def get_vals(self):
-    y = "["
-    for x in BoolQuestion.objects.all().order_by('id'):
-        # Starting as Neutral but need to check
-        # if user has voted before
-        y += " Voting <| VotingQuestion \"%s\" \"%s\" %d \"%s\" Neutral" % (x.title, x.prompt, x.score, x.id)
-        y += "\n"
-        y += ","
-    for x in MCQuestion.objects.all():
-        y += "MC <| MultipleChoiceQuestion \"%s\" \"%s\" \"%s\" MultipleChoiceOption " % (x.id, x.title, x.prompt, )
-        for z in [MCOption.objects.get(question_id=x.id)]:
-            y += "%s " % z.option
-        y += "\n"
-        y += ","
-    y += "]"
-    print(html.unescape(y))
-    self.y = html.unescape(y)
-
-
   def get(self, request):
-    #self.get_vals()
     voting_questions = BoolQuestion.objects.all()
     elm_flags = {
       'score': 42,
@@ -76,8 +55,6 @@ class HomeView(View):
       'voting_questions': voting_questions
     }
     context = {
-      #'elm_flags': elm_flags,
       'static_file': 'js/home.js'
     }
-    # return render(request, 'elm.html', context)
     return render_elm(request, 'elm.html', context, 'home.elm', elm_flags)
